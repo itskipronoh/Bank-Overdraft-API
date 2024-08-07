02ae5ea87ce11d7a6fb1f7f8ddd918b39afb79c3
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-07 19:54:49 +0300

    Update MongoDB connection configuration to use MONGODB_URI environment variable
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const overdraftAccounts = require('./routes/overdraftAccounts');
+const dotenv = require('dotenv');
+dotenv.config();
+
+connectDB();
+
+
 
 const app = express();
 
-// Connect Database
 connectDB();
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/api/overdraftAccounts', overdraftAccounts);
 
 const PORT = process.env.PORT || 5000;
--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(process.env.MONGODB_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
         });
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error(err.message);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error.message);
         process.exit(1);
     }
 };
