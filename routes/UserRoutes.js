5aac5ca68193f0af12fb2ddb3b818325f56280d0
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-08 20:02:26 +0300

    Update import statement for User model in UserRoutes
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Register a new user
+
 router.post('/register', userController.registerUser);
 
-// Login a user
 router.post('/login', userController.loginUser);
 
-// Get user profile (protected route)
+
 router.get('/profile', protect, userController.getUserProfile);
 
 module.exports = router;
