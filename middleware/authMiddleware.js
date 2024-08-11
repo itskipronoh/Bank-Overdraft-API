8576bc1b1cb26960b3099451cc27a0459334c2cb
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-11 22:08:06 +0300

    update middleware
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
     let token;
