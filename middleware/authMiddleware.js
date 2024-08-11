0400addd474244adb6a3ae26f87e36ed4673a30c
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-11 22:07:24 +0300

    add middleware
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+exports.protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (err) {
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
