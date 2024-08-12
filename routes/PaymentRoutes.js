f4bdc77f0ebc5b2b4d17454f45a698328270ddcf
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-12 20:50:39 +0300

    refactor payment routes
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- routes/PaymentRoutes.js
+++ routes/PaymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controllers/paymentController');
+const paymentController = require('../controllers/PaymentController');
 const { protect } = require('../middleware/authMiddleware');
 
 // Create a new payment
