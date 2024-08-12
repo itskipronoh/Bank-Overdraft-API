2996b42139bbbef1d920d56185f3a4a371039718
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-08-12 20:51:18 +0300

    refactor PaymentRoutes for better organization and readability
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- routes/PaymentRoutes.js
+++ routes/PaymentRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const paymentController = require('../controllers/PaymentController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Create a new payment
+
 router.post('/', protect, paymentController.createPayment);
 
-// Get all payments for a specific overdraft account
+
 router.get('/account/:accountId', protect, paymentController.getPaymentsByAccount);
 
-// Get a single payment by ID
+
 router.get('/:id', protect, paymentController.getPaymentById);
 
-// Delete a payment
+
 router.delete('/:id', protect, paymentController.deletePayment);
 
 module.exports = router;
